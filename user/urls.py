964ac62a7d4d3df0135a93cd97ebd1ced9fbed2d
Sudipta Banik <sudipta.banik.112@gmail.com>
2018-10-22 22:19:54 +0530

    Updated urls.py with 'home/' path to index
    for hacktoberfest 18

--- user/urls.py
+++ user/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-
+    path('home/' , views.index , name='index'),
     path('signup/', views.SignUp.as_view(), name = 'signup'),
     url(r'^(?P<id>\d+)/$', views.user),
     url(r'^detail/(?P<id>\d+)/$', views.User_detail),
